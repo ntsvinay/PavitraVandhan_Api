356a1cc6854595750aee312cf7470d62bd8fecf8
vinay mohniya <ntsvinay@gmail.com>
2020-06-06 18:28:19 +0530

    merging

--- pavitrabandhan/src/main/java/com/pavitrabandhan/PavitrabandhanApplication.java
+++ pavitrabandhan/src/main/java/com/pavitrabandhan/PavitrabandhanApplication.java
@@ -2,7 +2,6 @@ package com.pavitrabandhan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;

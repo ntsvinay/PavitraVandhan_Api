69c8d02cdfdd5419122461f881660c918d83a58c
vinay mohniya <ntsvinay@gmail.com>
2020-06-06 18:33:27 +0530

    test

--- pavitrabandhan/src/main/java/com/pavitrabandhan/PavitrabandhanApplication.java
+++ pavitrabandhan/src/main/java/com/pavitrabandhan/PavitrabandhanApplication.java
@@ -2,6 +2,7 @@ package com.pavitrabandhan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -33,4 +34,8 @@ public class PavitrabandhanApplication  extends SpringBootServletInitializer{
 			}
 		};
 	}
+	@Override
+	  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+	      return builder.sources(PavitrabandhanApplication.class);
+	  }
 }
